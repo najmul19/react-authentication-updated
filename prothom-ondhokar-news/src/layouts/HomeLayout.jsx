a1f15e152b0c004f90d6237c8bed3396393b5867
najmul <mdnajmulislam10992@gmail.com>
2025-04-16 02:52:59 +0600

    design navigation and set main layout with 12 grid and divide col spane

--- prothom-ondhokar-news/src/layouts/HomeLayout.jsx
+++ prothom-ondhokar-news/src/layouts/HomeLayout.jsx
@@ -1,18 +1,27 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
-
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
-    return (
-        <div  data-theme="light">
-            <header><Header></Header></header>
-            <section className="w-11/12 mx-auto">
-                <LatestNews></LatestNews>
-            </section>
-            <nav></nav>
-            <main></main>
-        </div>
-    );
+  return (
+    <div data-theme="light" className="font-poppins">
+      <header>
+        <Header></Header>
+        <section className="w-11/12 mx-auto">
+          <LatestNews></LatestNews>
+        </section>
+      </header>
+
+      <nav className="w-11/12 mx-auto py-2">
+        <Navbar></Navbar>
+      </nav>
+      <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+        <aside className="left col-span-3">Left navbar</aside>
+        <section className="col-span-6">Main Content</section>
+        <aside className="col-span-3">Right navba</aside>
+      </main>
+    </div>
+  );
 };
 
 export default HomeLayout;
