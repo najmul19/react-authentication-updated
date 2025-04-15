d32d7119a703fb80f424540cdbbfd68b9e1e3a41
najmul <mdnajmulislam10992@gmail.com>
2025-04-15 15:22:17 +0600

    create a project for auth context private route and basic setup

---
+++ auth-context-private-route/src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1JMI6KU9oqtG069gOe-R6014cTlPnO0s",
+  authDomain: "auth-context-private-rou-81913.firebaseapp.com",
+  projectId: "auth-context-private-rou-81913",
+  storageBucket: "auth-context-private-rou-81913.firebasestorage.app",
+  messagingSenderId: "397867191885",
+  appId: "1:397867191885:web:a9af6e6f41ce61996b9604"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
