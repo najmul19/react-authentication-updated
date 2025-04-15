ac6a9ba2ca8db15fdefe731d429011534b01b7e0
najmul <mdnajmulislam10992@gmail.com>
2025-04-15 15:48:13 +0600

    create login and register form

--- auth-context-private-route/src/componets/Login/Login.jsx
+++ auth-context-private-route/src/componets/Login/Login.jsx
@@ -1,11 +1,60 @@
-
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login</h2>
+    const handleLogin=(e)=>{
+        e.preventDefault();
+        const email = e.target.email.value
+        const password = e.target.password.value;
+        console.log(email,password);
+
+    }   
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col ">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+              name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+              name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+          <p className="ml-4 mb-4">New To this website? Please <Link to='/register' className="text-blue-600">Register</Link> </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- auth-context-private-route/src/componets/Register/Register.jsx
+++ auth-context-private-route/src/componets/Register/Register.jsx
@@ -1,11 +1,74 @@
-
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            <h3>Register</h3>
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const name = e.target.name.value;
+    console.log(name,email,password)
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col ">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                name="name"
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+               
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </form>
+          <p className="ml-4 mb-4">
+            Already Have an Account? Please{" "}
+            <Link to="/login" className="text-blue-600">
+            login
+            </Link>{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
