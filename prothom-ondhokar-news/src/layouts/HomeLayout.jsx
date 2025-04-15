b10ccee027450ce261254c2040219207aab6a0e2
najmul <mdnajmulislam10992@gmail.com>
2025-04-16 01:16:33 +0600

    router setup and folder structer complete

---
+++ prothom-ondhokar-news/src/layouts/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header><Header></Header></header>
+            <nav></nav>
+            <main></main>
+        </div>
+    );
+};
+
+export default HomeLayout;
