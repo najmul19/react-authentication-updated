72219b7d4f58016b13edd227048006ca5cdec5a3
najmul <mdnajmulislam10992@gmail.com>
2025-04-16 22:13:54 +0600

    design leftNavbar and fetch categories and show

---
+++ prothom-ondhokar-news/src/components/Layout-componet/LeftNavbar.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { data } from "react-router-dom";
+
+const LeftNavbar = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("https://openapi.programming-hero.com/api/news/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data.data.news_category));
+  }, []);
+  return (
+    <div>
+      <h2 className="font-semibold mb-3">All Category({categories.length})</h2>
+      <div className="flex flex-col gap-2 ">
+        {categories.map((cat) => (
+          <button className="btn " key={cat.category_id}>{cat.category_name}</button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftNavbar;
--- prothom-ondhokar-news/src/layouts/HomeLayout.jsx
+++ prothom-ondhokar-news/src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/Layout-componet/leftNavbar";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
@@ -16,7 +17,9 @@ const HomeLayout = () => {
         <Navbar></Navbar>
       </nav>
       <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
-        <aside className="left col-span-3">Left navbar</aside>
+        <aside className="left col-span-3">
+          <LeftNavbar></LeftNavbar>
+        </aside>
         <section className="col-span-6">Main Content</section>
         <aside className="col-span-3">Right navba</aside>
       </main>
