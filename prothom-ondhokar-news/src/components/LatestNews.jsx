35da2b9eb1cdcc882a0fd13960fe40ce57668b14
najmul <mdnajmulislam10992@gmail.com>
2025-04-18 02:09:08 +0600

    update the latest news with real link and show this in details page

--- prothom-ondhokar-news/src/components/LatestNews.jsx
+++ prothom-ondhokar-news/src/components/LatestNews.jsx
@@ -1,14 +1,20 @@
+import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
-import { Link } from "react-router-dom";
+import { data, Link, useNavigate } from "react-router-dom";
 
 const LatestNews = () => {
+    const [allNews,setAllNews] = useState([]);
+    useEffect(() => {
+        fetch("https://openapi.programming-hero.com/api/news/category/01")
+          .then((res) => res.json())
+          .then((data) => setAllNews(data.data));
+      }, []);
+      const navigate = useNavigate()
     return (
         <div className="flex gap-2 items-center bg-base-300 p-2">
             <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
             <Marquee pauseOnHover={true} speed={100} className="space-x-10">
-                <Link to='/news'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, pariatur!</Link>
-                <Link to='/news'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, pariatur!</Link>
-                <Link to='/news'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, pariatur!</Link>
+                {allNews.map(news=> <Link className="hover:bg-gray-300 p-2 hover:rounded-full" key={news._id} to={`/news/${news._id}`}>{news.title}</Link> )}
             </Marquee>
         </div>
     );
