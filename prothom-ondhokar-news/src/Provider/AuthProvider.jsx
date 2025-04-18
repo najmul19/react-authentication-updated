b4ad894969e4175ebd4c6ce8d725856a6be73388
najmul <mdnajmulislam10992@gmail.com>
2025-04-18 11:13:30 +0600

    implement firebase google sign in method

--- prothom-ondhokar-news/src/Provider/AuthProvider.jsx
+++ prothom-ondhokar-news/src/Provider/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import {
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
   updateProfile,
 } from "firebase/auth";
@@ -12,25 +14,30 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading,setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
+  const googleProvider = new GoogleAuthProvider();
   // console.log(loading ,user);
   const createNewUser = (email, password) => {
-    setLoading(true)
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const logOut = () => {
-    setLoading(true)
+    setLoading(true);
     return signOut(auth);
   };
   const sigIn = (email, password) => {
-    setLoading(true)
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
-  const updateUserProfile=(updatedData)=>{
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const updateUserProfile = (updatedData) => {
     return updateProfile(auth.currentUser, updatedData);
-  }
+  };
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -49,7 +56,7 @@ const AuthProvider = ({ children }) => {
     loading,
     setLoading,
     updateUserProfile,
-
+    googleSignIn,
   };
 
   return (
--- prothom-ondhokar-news/src/components/SocialLogin.jsx
+++ prothom-ondhokar-news/src/components/SocialLogin.jsx
@@ -1,11 +1,25 @@
 
+import { useContext } from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import { AuthContext } from "../Provider/AuthProvider";
+
 const SocialLogin = () => {
+    const {googleSignIn,setUser} = useContext(AuthContext);
+    const handleGoogleSignIn=()=>{
+        googleSignIn()
+        .then(res=>{
+            // console.log(res.user)
+            setUser(res.user);
+        })
+        .catch(e=>{
+            
+        })
+    }
     return (
         <div>
             <h2 className="font-semibold mb-3">Login With</h2>
             <div className=" *:w-full space-y-2">
-                <button className="btn">
+                <button onClick={handleGoogleSignIn} className="btn">
                     <FaGoogle></FaGoogle> Login With Google
                 </button>
                 <button className="btn">
